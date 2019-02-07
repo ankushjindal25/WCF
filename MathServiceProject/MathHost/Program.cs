7448fcc632ed410f18903452b5ec3df9fccc18ef
ankushjindal25 <41601700+ankushjindal25@users.noreply.github.com>
2019-02-07 21:11:02 +0530

    Add files via upload

---
+++ MathServiceProject/MathHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using MathService;
+namespace MathHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(MathService.MathService))) {
+                host.Open();
+                Console.WriteLine("Service Started {0}",DateTime.Today.ToString());
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ MathServiceProject/MathService/MathService.svc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MathService
+{
+    
+    public class MathService : IMathService
+    {
+
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiple(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
